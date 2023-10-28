1498014e1bd6bbc49752873b89b0aa27f1191b13
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2023-10-28 15:33:55 +0800

    Update
    albums.html and script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//navigation menu
+
+var navLinks = document.getElementById("navLinks");
+
+
+function hideMenuOnLoad() {
+    navLinks.style.right = "-200px";
+}
+
+window.addEventListener('load', hideMenuOnLoad);
+
+function openMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
