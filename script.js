544806581697ee204b8665199d893b257d189921
haelilac <shanacarmellabolok@gmail.com>
2023-11-01 02:03:46 +0800

    Updated code
    Added functions:
    Highlighted song title
    fixed progress bar

--- script.js
+++ script.js
@@ -16,164 +16,81 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 
-// Pause and Play of audio
-let progress = document.getElementById("progress");
+// Audio Function
 
-var audioElements = [
-    document.getElementById("SQAlbum"),
-    document.getElementById("KTLAlbum"),
-    document.getElementById("SOLOAlbum"),
-    document.getElementById("RAlbum"),
-    document.getElementById("BPAlbum"),
-    document.getElementById("MEAlbum"),
-    document.getElementById("LLAlbum"),
-];
+document.querySelectorAll('.play-button').forEach(function (image) {
+    var albumId = image.id + 'Album';
+    var audioElement = document.getElementById(albumId);
+    var progress = document.getElementById('progress' + image.id);
 
-var playButtons = [
-    document.getElementById("SQ"),
-    document.getElementById("KTL"),
-    document.getElementById("SOLO"),
-    document.getElementById("R"),
-    document.getElementById("BP"),
-    document.getElementById("ME"),
-    document.getElementById("LL"),
-];
+    var songIntervals = {
+        SQAlbum: [
+            { id: "ddudu", start: 0.4, end: 186 },
+            { id: "foreverYoung", start: 186, end: 386 },
+            { id: "really", start: 386, end: 590 },
+            { id: "seeULater", start: 590, end: 840 }
+        ],
+        KTLAlbum: [
+            { id: "killThislove", start: 0.4, end: 189 },
+            { id: "DKWtD", start: 189, end: 390 },
+            { id: "kickIt", start: 390, end: 580 },
+            { id: "hopeNot", start: 580, end: 768 },
+            { id: "dduduRemix", start: 768, end: 976 }
+        ]
+    };
+    var currentAlbum = albumId;
 
-var currentAudio = null; // To keep track of the currently playing audio
-
-function stopAllAudio() {
-    audioElements.forEach(function (audio, index) {
-        audio.pause();
-        playButtons[index].innerHTML = "Play";
-    });
-}
-
-playButtons.forEach(function (button, index) {
-    button.addEventListener("click", function () {
-        if (currentAudio !== audioElements[index]) {
-            stopAllAudio();
-            audioElements[index].play();
-            currentAudio = audioElements[index];
-            button.innerHTML = "Pause";
-            
-            // Update progress.max when a new audio starts playing
-            progress.max = currentAudio.duration;
-        } else {
-            if (currentAudio.paused) {
-                currentAudio.play();
-                button.innerHTML = "Pause";
-            } else {
-                currentAudio.pause();
-                button.innerHTML = "Play";
+    function stopAllAudio() {
+        document.querySelectorAll('audio').forEach(function (audio) {
+            if (audio !== audioElement) {
+                audio.pause();
             }
+        });
+    }
+
+    image.addEventListener('click', function () {
+        if (audioElement.paused) {
+            stopAllAudio(); // Stop all other audio elements
+            audioElement.play();
+        } else {
+            audioElement.pause();
         }
+    });
+
+    audioElement.addEventListener('timeupdate', function () {
+        var currentTime = audioElement.currentTime;
+        var duration = audioElement.duration;
+        progress.value = (currentTime / duration) * 100;
+
+        songIntervals[currentAlbum].forEach(function (song) {
+            var element = document.getElementById(song.id);
+            if (currentTime >= song.start && currentTime <= song.end) {
+                element.style.color = "red";
+            } else {
+                element.style.color = "black";
+            }
+        });
+    });
+
+    audioElement.addEventListener('loadedmetadata', function () {
+        progress.max = audioElement.duration;
+    });
+
+    progress.addEventListener('input', function () {
+        var newTime = (progress.value / 100) * audioElement.duration;
+        audioElement.currentTime = newTime;
+    });
+
+    audioElement.addEventListener('ended', function () {
+        progress.value = 0;
+        songIntervals[currentAlbum].forEach(function (song) {
+            var element = document.getElementById(song.id);
+            element.stylecolor = "black";
+        });
     });
 });
 
-audioElements.forEach(function (audio, index) {
-    audio.addEventListener("timeupdate", function () {
-        if (!progress.getAttribute("seeking")) {
-            progress.value = audio.currentTime;
-            if (!audio.paused) {
-                progress.value = audio.currentTime;
-            }
-        }
-    });
 
-    audioElements.forEach(function (audio, index) {
 
-        audio.addEventListener("loadedmetadata", function () {
-            progress.max = audio.duration;
-        });
-    
 
-        audio.addEventListener("timeupdate", function () {
-            if (!progress.getAttribute("seeking")) {
-                progress.value = audio.currentTime;
-            }
-        });
 
-        audio.addEventListener("ended", function () {
-            progress.value = 0; // Reset progress bar to start when audio ends
-        });
-    });
-
-    progress.addEventListener("input", function () {
-        if (currentAudio) { // Check if an audio is playing
-            currentAudio.currentTime = progress.value;
-        }
-    });
-    //ddudu
-    var dduduStartTime = 0;
-    var dduduEndTime = 208;
-    var dduduTextColor = "red";
-
-    var dduduElement = document.getElementById("ddudu");
-    var audioElement = document.getElementById("SQAlbum");
-
-    audioElement.addEventListener("timeupdate", function () {
-        progress.value = audioElement.currentTime;
-
-        // Check the current time and apply or remove the text color accordingly
-        if (audioElement.currentTime >= dduduStartTime && audioElement.currentTime <= dduduEndTime) {
-            dduduElement.style.color = dduduTextColor;
-        } else {
-            dduduElement.style.color = "black";
-        }
-    });
-
-    var foreverYoungStartTime = 208.9;
-    var foreverYoungEndTime = 445.9;
-    var foreverYoungTextColor = "red";
-
-    var foreverYoungElement = document.getElementById("foreverYoung");
-    var audioElement = document.getElementById("SQAlbum");
-
-    audioElement.addEventListener("timeupdate", function () {
-        progress.value = audioElement.currentTime;
-
-        // Check the current time and apply or remove the text color accordingly
-        if (audioElement.currentTime >= foreverYoungStartTime && audioElement.currentTime <= foreverYoungEndTime) {
-            foreverYoungElement.style.color = foreverYoungTextColor;
-        } else {
-            foreverYoungElement.style.color = "black";
-        }
-    });
-    //really
-    var reallyStartTime = 446;
-    var reallyEndTime = 643.9;
-    var reallyTextColor = "red";
-
-    var reallyElement = document.getElementById("really");
-    var audioElement = document.getElementById("SQAlbum");
-
-    audioElement.addEventListener("timeupdate", function () {
-        progress.value = audioElement.currentTime;
-
-        // Check the current time and apply or remove the text color accordingly
-        if (audioElement.currentTime >= reallyStartTime && audioElement.currentTime <= reallyEndTime) {
-            reallyElement.style.color = reallyTextColor;
-        } else {
-            reallyElement.style.color = "black";
-        }
-    });
-    
-    var seeULaterStartTime = 644;
-    var seeULaterEndTime = 838;
-    var seeULaterTextColor = "red";
-
-    var seeULaterElement = document.getElementById("seeULater");
-    var audioElement = document.getElementById("SQAlbum");
-
-    audioElement.addEventListener("timeupdate", function () {
-        progress.value = audioElement.currentTime;
-
-        // Check the current time and apply or remove the text color accordingly
-        if (audioElement.currentTime >= seeULaterStartTime && audioElement.currentTime <= seeULaterEndTime) {
-            seeULaterElement.style.color = seeULaterTextColor;
-        } else {
-            seeULaterElement.style.color = "black";
-        }
-    });
-    
-})
